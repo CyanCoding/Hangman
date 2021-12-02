acb65d1ac09ac44569f69a1180f6d3323a9bb8ec
Camden Mac Leod <camden.macleod@pm.me>
2021-12-02 18:30:55 -0500

    Made it work

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include <iostream>             // std::cout
 #include <vector>               // std::vector
 #include <filesystem>           // std::filesystem
-#include <fstream>             // std::ifstream
+#include <fstream>              // std::ifstream
+#include <algorithm>            // ::tolower
 
 std::vector<std::string> getDictionary(int input) {
     std::string dictionaryFile;
@@ -32,7 +33,9 @@ std::vector<std::string> getDictionary(int input) {
 }
 
 int main() {
+    srand(time(0)); // Used for random generation of numbers
 
+    std::cout << "\033[2J\033[1;1H"; // Clears the screen in Linux and Windows
     std::cout << "Hangman - a C++ project by CyanCoding." << std::endl;
     std::cout << "More categories coming soon!" << std::endl << std::endl;
     std::cout << "Please select a category:" << std::endl;
@@ -44,9 +47,68 @@ int main() {
 
     std::vector<std::string> dictionary = getDictionary(std::stoi(input));
 
-    for (std::string i : dictionary) {
-        std::cout << i << std::endl;
+    // Get the word to use and convert it to lowercase
+    std::string selectedWord = dictionary[(rand() % dictionary.size() - 1)];
+    transform(selectedWord.begin(), selectedWord.end(), selectedWord.begin(), ::tolower);
+
+    int wordLength = selectedWord.size();
+
+    // Used to represent the word
+    std::string guess = "";
+    for (int i = 0; i < wordLength; i++) {
+        guess += "-";
     }
+
+    int triesLeft = 8;
+    std::string lettersUsed;
+    // Our repetitive loop until they run out of guesses or win
+    while (triesLeft != 0) {
+        bool correctLetter = false;
+
+        std::cout << "\033[2J\033[1;1H"; // Clears the screen in Linux and Windows
+        std::cout << "Current progress: " << guess << std::endl;
+        std::cout << "Attempts left: " << triesLeft << std::endl;
+        std::cout << "Letters used: " << lettersUsed << std::endl;
+
+        std::cout << std::endl << "Please enter a character to guess > ";
+
+        char letter;
+        std::cin >> letter;
+
+        for (int i = 0; i < wordLength; i++) {
+            if (selectedWord[i] == letter) {
+                guess[i] = letter;
+                correctLetter = true;
+            }
+        }
+
+        // We don't penalize them if they guess something
+        // they have already guessed before
+        bool alreadyGuessed = false;
+        for (char l : lettersUsed) {
+            if (l == letter) {
+                alreadyGuessed = true;
+            }
+        }
+        if (!alreadyGuessed) {
+            if (!correctLetter) {
+                triesLeft--;
+            }
+            lettersUsed += letter;
+        }
+
+        if (guess == selectedWord) {
+            triesLeft = 0;
+        }
+    }
+
+    if (guess == selectedWord) {
+        std::cout << std::endl << "Congratulations! You solved the word " << selectedWord << std::endl;
+    }
+    else {
+        std::cout << std::endl << "Sorry! You failed. The word was " << selectedWord << std::endl;
+    }
+
 
     return 0;
 }
